358e688baa12fb47b54246365a6c148779d306f6
David Adams <david.riding.rails@gmail.com>
2019-04-10 15:34:19 -0700

    Add Meal model tests

--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MealTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @valid_meal = meals(:one)
+    @invalid_meal = meals(:no_title)
+  end
+
+  test "Meal with title is valid" do
+    assert @valid_meal.valid?
+  end
+
+  test "Meal without title is not valid" do
+    assert_not @invalid_meal.valid?
+  end
+
 end
